01329825f753a379aad326824477c90a0a4ce51e
Kiet A1V <trnanhkiet@gmail.com>
2024-08-30 14:29:38 +0700

    everything except IV

--- src/universal/Gen1Convert.c
+++ src/universal/Gen1Convert.c
@@ -30,14 +30,17 @@ void convert_bank3_to_gen1(int box, int slot)
     for (int i_field = 1; i_field <= 44; i_field++)
     {
 
-        snprintf(field_id_buffer, sizeof(field_id_buffer), "Convert PKX Field : %d/44", i_field);
-        gui_warn(field_id_buffer);
+        // snprintf(field_id_buffer, sizeof(field_id_buffer), "Convert PKX Field : %d/44", i_field);
+        // gui_warn(field_id_buffer);
 
         switch (i_field)
         {
         case MOVE: // special case (two values in field)
-        case PP:
+                   // move: 0 -> 165
+
+        case PP: // always in range 0 -> 40
         case PP_UPS:
+
             for (int m = 0; m < 4; m++)
             {
                 if (!pkx_get_value(pkm_g3, GEN_THREE, i_field, m))
@@ -46,24 +49,18 @@ void convert_bank3_to_gen1(int box, int slot)
                     break;
                 }
 
+                int skill_id = pkx_get_value(
+                    pkm_g3,
+                    GEN_THREE,
+                    i_field,
+                    m);
+
                 pkx_set_value(pkm_g1,
                               GEN_ONE,
                               i_field,
-                              m,
-                              pkx_get_value(
-                                  pkm_g3,
-                                  GEN_THREE,
-                                  i_field,
-                                  m));
+                              m, skill_id > 165 ? 0 : skill_id);
             }
             is_inject = false;
-            break;
-
-        case LEVEL:
-            pkx_set_value(pkm_g1,
-                          GEN_ONE,
-                          LEVEL, pkx_get_value(pkm_g3, GEN_THREE, LEVEL));
-            is_inject = true;
             break;
 
         case POKERUS: // field that gen_1 or both gen1 & gen3 doesn't have
@@ -76,9 +73,18 @@ void convert_bank3_to_gen1(int box, int slot)
         case MET_DAY:
         case MET_MONTH:
         case MET_YEAR:
+        case SPECIES:
         case NATURE:
         case MET_LEVEL:
         case ORIGINAL_GAME:
+
+        case IV_HP:
+        case IV_ATK:
+        case IV_DEF:
+        case IV_SPATK:
+        case IV_SPDEF:
+        case IV_SPEED:
+
             is_inject = false;
             break;
 
